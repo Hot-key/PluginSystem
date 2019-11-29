1e2b36b6b2f455f9d2a94ad4f66bc32a9cd37933
Hot-key <NanoHotkey@protonmail.ch>
2019-11-29 10:45:23 +0900

    초기 커밋.

---
+++ PluginSystem/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PluginSystem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IPrintAble printSystem = LoadItem<IPrintAble>(Path.GetFullPath("./PluginTemp.dll"), "MyPrint", "This Class In PluginTemp");
+
+            Console.WriteLine(printSystem.PrintData());
+        }
+
+        public static T LoadItem<T>(string fileName, string name, params object[] initData)
+        {
+            Type interfaceType = typeof(T);
+
+            foreach (var type in System.Reflection.Assembly.LoadFile(fileName).GetTypes())
+            {
+                if (interfaceType.IsAssignableFrom(type) && interfaceType != type && type.Name == name)
+                {
+                    T instance = (T)Activator.CreateInstance(type, initData);
+
+                    return instance;
+                }
+            }
+            return default;
+        }
+    }
+
+    public interface IPrintAble
+    {
+        string PrintData();
+    }
+}
---
+++ PluginTemp/MyPrint.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PluginSystem;
+
+namespace PluginTemp
+{
+    public class MyPrint : IPrintAble
+    {
+        private readonly string data;
+
+        public MyPrint(string data)
+        {
+            this.data = DateTime.Now.ToString("[yyyy-MM-dd HH:mm:ss] ") + data;
+        }
+
+        public string PrintData() => data;
+    }
+}
